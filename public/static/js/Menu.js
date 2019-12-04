d50299662f27e73e89bfbb8049ec9a4b694c0670
LeandrodaSilva <ld_silva13@hotmail.com>
2019-12-03 23:28:20 -0300

    First commit

---
+++ public/static/js/Menu.js
@@ -0,0 +1,94 @@
+
+export class MenuElements{
+  uiFood(name){
+    let $inputName = document.createElement('input');
+    $inputName.type = 'text';
+    $inputName.classList.add('form-control');
+    $inputName.readOnly = true;
+    $inputName.value = name;
+
+    let $nameSpan = document.createElement('span');
+    $nameSpan.classList.add('input-group-text');
+    $nameSpan.id = 'basic-addon1';
+    $nameSpan.textContent = 'Nome';
+
+    let $spanDiv = document.createElement('div');
+    $spanDiv.classList.add('input-group-prepend');
+    $spanDiv.appendChild($nameSpan);
+
+    let $externalDiv = document.createElement('div');
+    $externalDiv.classList.add('input-group', 'mb-3');
+    $externalDiv.appendChild($spanDiv);
+    $externalDiv.appendChild($inputName);
+
+    return $externalDiv;
+  }
+}
+
+
+export default class Menu {
+  _(){
+    this._$element = null;
+    this._$header = null;
+    this._$menuElements = null;
+  }
+
+
+  constructor($element, $header){
+    this._();
+    this._$element = $element;
+    this._$header = $header;
+    this._$menuElements = new MenuElements();
+
+    let $btnSearch = $header.querySelector('#btn-search');
+
+    $btnSearch.addEventListener('click', (event) =>{
+      event.preventDefault();
+      this._listar();
+      // this._teste();
+    });
+
+    let $inputSearch = $header.querySelector('#input-search');
+
+    $inputSearch.addEventListener('keyup', (e) => {
+      e.preventDefault();
+      // console.log($inputSearch.value);
+      this._buscar($inputSearch.value);
+    });
+  }
+
+  _teste(){
+    console.log(this._$menuElements.uiFood('teste'));
+  }
+
+  _buscar(nome){
+    $.get("http://localhost:3000/menu/?name="+nome, function (data) {
+      console.log(data)
+    })
+  }
+
+  _listar(){
+    let $inputSearch = this._$header.querySelector('#input-search');
+    let inputValue = $inputSearch.value;
+    let dados = null;
+
+    if (inputValue !== '') {
+      $.get("http://localhost:3000/menu/"+inputValue, (item) => this._criaUmItem(item));
+    } else {
+      // Swal.fire('Não inseriu um valor');
+      $.get("http://localhost:3000/menu/", (itens) => this._criaVariosItens(itens));
+    }
+  };
+
+  _criaUmItem(item) {
+    this._$element.appendChild(this._$menuElements.uiFood(item.name));
+  }
+
+  _criaVariosItens(dados) {
+    dados.forEach((dado) => {
+      console.log(dado.name);
+      let $food = this._$menuElements.uiFood(dado.name);
+      this._$element.append($food);
+    });
+  }
+}
